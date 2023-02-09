b55a13ceccd5d00e175a990ff84e441e8312b3a9
Timadey <dlktimothy@gmail.com>
2023-02-10 00:56:20 +0100

    Change file name to 0-uniq_users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++
@@ -1,3 +0,0 @@
--- Create a table user with some fields
--- Create table with column id, email and country
-CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US' );
