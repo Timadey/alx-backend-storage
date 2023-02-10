4ca4d85b51f47c290ab9a874ba9ebeaba948816c
Timadey <dlktimothy@gmail.com>
2023-02-10 16:24:19 +0100

    Edit query for selecting Glam rock stylek bands

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,17 +1,4 @@
--- Calculate life span of band 
--- Define a Common Table Expression (CTE) to calculate the lifespan of each band
-WITH band_lifespan AS (
-  SELECT 
-    band_name, 
-    -- Calculate the lifespan as the difference between the split and formed attributes
-    (split - formed) AS lifespan 
-  FROM metal_bands 
-  -- Filter for bands with Glam Rock as their main style
-  WHERE main_style = 'Glam Rock'
-)
--- Select the band name and lifespan from the CTE, and order the result by lifespan in descending order
-SELECT 
-  band_name, 
-  lifespan 
-FROM band_lifespan 
-ORDER BY lifespan DESC;
+-- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+-- Calculate the select the bands and calculate their lifespan
+SELECT band_name, (formed - split) AS lifespan
+FROM metal_bands WHERE style = 'Glam rock';
