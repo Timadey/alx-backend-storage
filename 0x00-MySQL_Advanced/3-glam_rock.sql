cd6ae8c23043725375c7625ae46c42474e9dbb28
Timadey <dlktimothy@gmail.com>
2023-02-10 17:15:42 +0100

    Change year for split if NULL

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
 -- Calculate the select the bands and calculate their lifespan
 SELECT band_name,
-IFNULL (split, 2020) - formed AS lifespan
+IFNULL (split, YEAR(CURDATE())) - formed AS lifespan
 FROM metal_bands WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
