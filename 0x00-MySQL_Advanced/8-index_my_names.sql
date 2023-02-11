e66020c5af48c854f7550b700ffc62d6723e3e6a
Timadey <dlktimothy@gmail.com>
2023-02-11 09:46:36 +0100

    Edit index for name

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,4 @@
 -- SQL script that creates an index
 -- idx_name_first on the table names and the first letter of name
 CREATE INDEX idx_name_first
-ON names (SELECT LEFT(name, 1) AS `name` FROM `names`.`name`);
+ON names (SUBSTR(name, 1, 1));
