f771b0c5055de580161352905525f299d367d60e
Timadey <dlktimothy@gmail.com>
2023-02-14 06:26:39 +0100

    Fix errors

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,7 +2,6 @@
 """function that changes all topics of a school document based on the name
 """
 
-
 def update_topics(mongo_collection, name, topics):
     """mongo_collection: a pymango collection object
     name: (string) the schol name to update
