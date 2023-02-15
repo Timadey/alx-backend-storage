15bf513c1be9b9e7155f21d72eb6784f05aa51c7
Timadey <dlktimothy@gmail.com>
2023-02-15 03:08:56 +0100

    Add function documentation

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -5,6 +5,11 @@ Function that returns all students sorted by average score
 
 
 def top_students(mongo_collection):
+    """
+    Compute the average score of students on each topic and set a new
+    field `averageScore in the document`
+    mongo_collection: a pymongo collection object
+    """
     result = mongo_collection.aggregate([
         {
             '$set': {
