95f86acba5cea8bd2e5a68639827fb815f2f66e2
Timadey <dlktimothy@gmail.com>
2023-02-15 19:23:32 +0100

    Add Redis Exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,8 +3,22 @@
 Module for exercises relating to REDIS Basics
 """
 import redis
-from typing import Union
 import uuid
+from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator that counts how many times methods
+    of the Cache class are called
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        result = method(self, *args, **kwargs)
+        self._redis.incr(method.__qualname__)
+        return result
+    return wrapper
 
 
 class Cache():
@@ -18,13 +32,34 @@ class Cache():
         Flush the instance
         """
         self._redis = redis.Redis()
-        self._redis.flushdb
+        self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Generates a random key, store the input date in Redis using
         the random key and return the key
         """
-        key = uuid.uuid4()
+        key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, int, bytes, None]:
+        """
+        Get a value from Redis with the given key
+        """
+        if fn is not None:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """
+        Automiatically parametrize Cache.get with str conversion function
+        """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """
+        Automiatically parametrize Cache.get with int conversion function
+        """
+        return self.get(key, int)
