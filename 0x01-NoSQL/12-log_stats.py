ddfa6e2d76dc5034442d91a14b1a02e02ac2debc
Timadey <dlktimothy@gmail.com>
2023-02-15 01:19:49 +0100

    Fix printing error

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,10 +6,11 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient()
     nginx = client.logs.nginx
-    print(f"""{nginx.count_documents({})} logs\nMethods:
-    method GET: {nginx.count_documents({'method': 'GET'})}
-    method POST: {nginx.count_documents({'method': 'POST'})}
-    method PUT: {nginx.count_documents({'method': 'PUT'})}
-    method PATCH: {nginx.count_documents({'method': 'PATCH'})}
-    method DELETE: {nginx.count_documents({'method': 'DELETE'})}
-{nginx.count_documents({'path': '/status'})} status check""")
+    print(f"{nginx.count_documents({})} logs\nMethods:")
+    print(f"\tmethod GET: {nginx.count_documents({'method': 'GET'})}")
+    print(f"\tmethod POST: {nginx.count_documents({'method': 'POST'})}")
+    print(f"\tmethod PUT: {nginx.count_documents({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {nginx.count_documents({'method': 'PATCH'})}")
+    print(
+        f"\tmethod DELETE: {nginx.count_documents({'method': 'DELETE'})}")
+    print(f"{nginx.count_documents({'path': '/status'})} status check")
